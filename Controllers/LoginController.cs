c3d097a248c31b0e419ffe8c1cd76206a105c98f
RodrigoDevChagas <dyggo.ag22@gmail.com>
2022-05-26 13:18:56 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace SistemadeLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logar(string username, string senha) 
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("Server=localhost;Database=usuariosdb;Uid=root;Password=dyggolekao123");
+            await mySqlConnection.OpenAsync();
+            
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT * FROM usuarios WHERE username = '{username}' AND senha = '{senha}'";
+           
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+           
+            if (await reader.ReadAsync()) 
+            {
+                
+                return Json(new { Msg = "Usuario logado com sucesso." });
+            }
+            return Json(new { Msg = "Usuario nao encontrado, verifique suas credenciais." });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemadeLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
